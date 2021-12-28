e46447263a966229502868b3205eac48bd180314
Dominic Robinson <dominic@dcrdev.com>
2021-12-28 12:27:43 +0000

    Add source tarball

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 DIST="fc35"
 ARCH="x86_64"
 MOCK_CONFIG="fedora-34-x86_64"
-SOURCE=`spectool -g -f -C ./ ./libva-vdpau-driver.spec | grep Downloaded | sed 's/Downloaded: //g'`
+SOURCE=`spectool -g -f -C ./ ./vdpau-va-driver-vp9.spec | grep Downloaded | sed 's/Downloaded: //g'`
 
 md5sum "$SOURCE" > sources
 
 mock -r "/etc/mock/$MOCK_CONFIG.cfg" --clean
 mock -r "/etc/mock/$MOCK_CONFIG.cfg" --init
 
-mock -r "/etc/mock/$MOCK_CONFIG.cfg" --spec=./libva-vdpau-driver.spec --sources=. --resultdir=./build/$DIST/SRPMS --buildsrpm && \
+mock -r "/etc/mock/$MOCK_CONFIG.cfg" --spec=./vdpau-va-driver-vp9.spec --sources=. --resultdir=./build/$DIST/SRPMS --buildsrpm && \
 mock -r "/etc/mock/$MOCK_CONFIG.cfg" rebuild ./build/$DIST/SRPMS/*.src.rpm --resultdir=./build/$DIST/$ARCH
